bfa02909ab572448f9f67972c10c416bfc71fc6d
Tom Byl <tomasz.bylina@edge1s.com>
2021-04-19 16:06:03 +0200

    initial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,36 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    public static WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.firefoxdriver().setup();
+    }
+
+    @Test(priority = 1)
+    public void setUp(){
+        driver = new FirefoxDriver();
+    }
+
+    @Test(priority = 2)
+    public void doLogin(){
+        driver.get("http://gmail.com");
+        driver.findElement(By.id("identifierId")).sendKeys("lol@qw.qw");
+        driver.findElement(By.cssSelector("#identifierNext > div > button")).sendKeys("haslo");
+    }
+
+    @AfterSuite
+    public void  tearDown(){
+        driver.quit();
+    }
+}
