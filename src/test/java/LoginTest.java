5313ec8d0c33575a06045ccfac41de465987f498
Tom Byl <tomasz.bylina@edge1s.com>
2021-04-19 19:28:02 +0200

    poprawki niedzialajacych testow 3

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,10 +3,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
 
@@ -27,6 +30,7 @@ public class LoginTest {
         driver.get("http://gmail.com");
         driver.findElement(By.id("identifierId")).sendKeys("lol@qw.qw");
         driver.findElement(By.cssSelector("#identifierNext > div > button")).sendKeys("haslo");
+        WebDriverWait wait = new WebDriverWait(driver, 10l);
     }
 
     @AfterSuite
